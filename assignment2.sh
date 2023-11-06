a7add3da9cdf9b82ed52a99c198b95e0abfab3c0
Lobarev Vadim <96525609+LobarevVadim@users.noreply.github.com>
2023-11-06 13:05:52 -0500

    commit
    just started, first template

---
+++ assignment2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Vadim Lobarev 200530998
+# Assignment 2
+
+# Function for displaying messages
+function display_message {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+
+# Network Configuration
+display_message "Testing and validation..."
+display_message "Configuring network..."
+
+
+# Software Installation
+display_message "Testing and validation..."
+display_message "Installing required software..."
+
+
+# Firewall Configuration
+display_message "Testing and validation..."
+display_message "Configuring firewall..."
+
+
+# User Account Creation
+display_message "Testing and validation..."
+display_message "Creating user accounts..."
+
+
+display_message "Script execution complete."
+
+# End of script
+
